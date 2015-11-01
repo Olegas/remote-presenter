51b51de9b4143277b1fe3e6fe49427492d320df5
Oleg Elifantiev <oleg@elifantiev.ru>
2015-11-01 22:43:07 +0300

    Init

---
+++ server.js
@@ -0,0 +1,17 @@
+var app = require('express')();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+var pat = require('path');
+
+server.listen(process.env.PORT);
+
+app.get('/', function (req, res) {
+   res.sendfile(path.join(__dirname, 'static/index.html'));
+});
+
+io.on('connection', function (socket) {
+   socket.emit('news', { hello: 'world' });
+   socket.on('my other event', function (data) {
+      console.log(data);
+   });
+});
