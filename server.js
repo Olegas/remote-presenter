61b7628223474309d40bc9b4a6cd06a480777040
Oleg Elifantiev <oleg@elifantiev.ru>
2015-11-01 22:45:46 +0300

    Typo fixed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var pat = require('path');
+var path = require('path');
 
 server.listen(process.env.PORT);
 
