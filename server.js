de241e110629d2454292ff147efea294150a6917
Oleg Elifantiev <oleg@elifantiev.ru>
2015-11-02 11:57:58 +0300

    Socketio removed

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 
 var app = require('express')();
 var server = require('http').Server(app);
-var io = require('socket.io')(server);
 var path = require('path');
 var WebSocketServer = require('ws').Server;
 var wss = new WebSocketServer({ server: server });
